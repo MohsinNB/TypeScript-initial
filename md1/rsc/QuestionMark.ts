acdf445b97c53705cff084c1b050f72b50d7728b
MOHAMMAD MOHSIN <mohammadmohsin28012005@gmail.com>
2025-05-07 06:39:39 +0600

    Unknown types

--- md1/rsc/QuestionMark.ts
+++ md1/rsc/QuestionMark.ts
@@ -21,4 +21,23 @@
   const isAuthenticated = returnUndefined();
   const result1 = isAuthenticated ?? "guest";
   console.log(result1);
+
+  type User = {
+    name: string;
+    address: {
+      city: string;
+      presentAddress?: string;
+      parmanentAddress?: string;
+    };
+  };
+  const user: User = {
+    name: "Mohamamad Mohsin",
+    address: {
+      city: "sylhet",
+      presentAddress: "uposhohor",
+    },
+  };
+  const parmanentAddress: string =
+    user?.address?.parmanentAddress ?? "There is no parmanent Address";
+  console.log(parmanentAddress);
 }
---
+++ md1/rsc/nullableTypes.ts
@@ -0,0 +1,26 @@
+{
+  // Nullable Types
+  const SearchThing = (value: string | null) => {
+    if (value) {
+      console.log("searching");
+    } else {
+      console.log("there is nothing to search");
+    }
+  };
+  SearchThing(null);
+
+  //   Unknown Types
+  const getSpeedInMeterPerSecond = (value: unknown) => {
+    if (typeof value == "number") {
+      const getResult = ((value * 1000) / 3600).toFixed(2);
+      console.log(`The speed is: ${getResult} ms^-1`);
+    } else if (typeof value == "string") {
+      const [result, unit] = value.split(" ");
+      const getResult = ((parseFloat(result) * 1000) / 3600).toFixed(2);
+      console.log(`The speed is: ${getResult} ms^-1`);
+    } else {
+      console.log("wrong Input");
+    }
+  };
+  getSpeedInMeterPerSecond("1000");
+}
